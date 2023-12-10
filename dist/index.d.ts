4f5bdd66e651d51bdc09aa4a086bb212372969b0
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2023-12-10 21:51:46 +0000

    enahanced lint-staged with git add

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
-import React from "react";
+/// <reference types="react" />
 interface Props {
     text: string;
 }
-export declare const ExampleComponent: ({ text }: Props) => React.JSX.Element;
+export declare const ExampleComponent: ({ text }: Props) => import("react").JSX.Element;
 export {};
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,10 +1,8 @@
-import React from 'react';
-
 var styles = {"test":"_3ybTi"};
 
 var ExampleComponent = function ExampleComponent(_ref) {
   var text = _ref.text;
-  return React.createElement("div", {
+  return /*#__PURE__*/React.createElement("div", {
     className: styles.test
   }, "Example Component: ", text);
 };
